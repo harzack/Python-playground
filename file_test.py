e7464836370219583a71d2d18973d02ed0e87a54
Alex <consultant@pierrekiroule.com>
2024-03-15 14:39:40 -0400

    updated the file test with proper code

--- file_test.py
+++ file_test.py
@@ -1,6 +1,25 @@
 __author__ = 'alex'
 
-test_file = open('/Users/alex/Developer/python/project01/test.txt', 'w')
-test_file.write('This line is added by Python')
-test_file.write('another line maybe')
-test_file.close()
+# with open('test.txt', 'r') as test_file_r:
+#     print(test_file_r.read())
+
+test_file_w = open('test.txt', 'a')
+
+''' Opening a file:
+Character   Meaning
+'r'         open for reading (default)
+'w'         open for writing, truncating the file first
+'x'         open for exclusive creation, failing if the file already exists
+'a'         open for writing, appending to the end of file if it exists
+'b'         binary mode
+'t'         text mode (default)
+'+'         open for updating (reading and writing)
+'''
+
+test_file_w.write("newer content: \n")
+test_file_w.write('This line is added by Python twice\n')
+test_file_w.write('another line thrice\n')
+test_file_w.close()
+
+# test_file.write(content)
+
